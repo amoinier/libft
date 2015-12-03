fe7785555b658af3b8e04b017e49d25d1683ea45
Alex MOINIER <amoinier@e2r8p3.42.fr>
2015-12-03 17:43:23 +0100

    memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: amoinier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 16:11:53 by amoinier          #+#    #+#             */
-/*   Updated: 2015/12/02 12:18:34 by amoinier         ###   ########.fr       */
+/*   Updated: 2015/12/03 17:34:42 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memalloc(size_t size)
 	size_t	i;
 
 	zmem = (void *)malloc(sizeof(*zmem) * size);
-	if (!zmem)
+	if (!zmem || size == 0)
 		return (NULL);
 	i = 0;
 	tmp = zmem;
