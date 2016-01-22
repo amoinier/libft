c4e4d408a1cebf8d937d60c7a39216c2082388d2
Alex MOINIER <amoinier@e2r7p4.42.fr>
2016-01-22 10:47:11 +0100

    ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,73 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amoinier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/25 18:27:31 by amoinier          #+#    #+#             */
-/*   Updated: 2016/01/06 16:42:13 by amoinier         ###   ########.fr       */
+/*   Created: 2016/01/22 10:25:53 by amoinier          #+#    #+#             */
+/*   Updated: 2016/01/22 10:45:36 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	char	*ft_dtrim(char const *s)
+char	*ft_strtrim(char const *s)
 {
-	int		i;
-	int		j;
-	char	*tmp;
-
-	i = 0;
-	j = 0;
-	if (!s)
-		return (NULL);
-	tmp = (char *)malloc(sizeof(tmp) * (ft_strlen(s) + 1));
-	if (tmp == NULL)
-		return (NULL);
-	while (s[i] == 32 || s[i] == '\n' || s[i] == '\t')
-	{
-		i++;
-	}
-	while (s[i + j])
-	{
-		tmp[j] = s[i + j];
-		j++;
-	}
-	tmp[j] = '\0';
-	return (tmp);
-}
-
-static	char	*ft_ltrim(char *s)
-{
+	char	*str;
 	int		i;
 	int		j;
 	int		len;
-	char	*tmp;
 
-	if (!s)
-		return (NULL);
-	len = ft_strlen(s) - 1;
 	i = 0;
 	j = 0;
-	tmp = (char *)malloc(sizeof(tmp) * (len + 1));
-	if (!tmp)
-		return (NULL);
-	while (s[len - i] == 32 || s[len - i] == '\n' || s[len - i] == '\t')
-	{
+	len = ft_strlen(s) - 1;
+	while (s[i] == 32 || s[i] == '\n' || s[i] == '\t')
 		i++;
-	}
-	len++;
-	while (j < len - i)
-	{
-		tmp[j] = s[j];
+	while (s[len - j] == 32 || s[len - j] == '\n' || s[len - j] == '\t')
 		j++;
+	if (i == 0 && j == 0)
+	{
+		str = ft_strdup(s);
+		return (str);
 	}
-	tmp[j] = '\0';
-	return (tmp);
-}
-
-char			*ft_strtrim(char const *s)
-{
-	if (!s)
+	if (i == len + 1)
+	{
+		str = "";
+		return (str);
+	}
+	if (!(str = ft_strsub(s, i, len - j - i + 1)))
 		return (NULL);
-	if (!ft_ltrim(ft_dtrim(s)))
-		return (NULL);
-	return (ft_ltrim(ft_dtrim(s)));
+	return (str);
 }
