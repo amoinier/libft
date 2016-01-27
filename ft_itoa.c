b8e5c7c388c117dbf3283d181009b2c59af78fd7
Alex MOINIER <amoinier@e2r7p6.42.fr>
2016-01-27 15:06:02 +0100

    ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amoinier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: amoinier <amoinier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 15:52:46 by amoinier          #+#    #+#             */
-/*   Updated: 2016/01/22 22:34:33 by amoinier         ###   ########.fr       */
+/*   Updated: 2016/01/27 15:05:15 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,36 @@ static	int		ft_val(int n)
 	return (i);
 }
 
+char			*ft_nbr(int n, int val, int i, char *tab)
+{
+	int	j;
+	int	power;
+
+	j = 0;
+	while (j < val)
+	{
+		power = val - (j) - 1;
+		tab[i] = 48 + (n / (ft_power(10, power)) % 10);
+		i++;
+		j++;
+	}
+	tab[i] = '\0';
+	return (tab);
+}
+
 char			*ft_itoa(int n)
 {
 	int		i;
 	int		j;
 	char	*tab;
-	int		power;
+	int		val;
 
 	i = 0;
 	j = 0;
-	if (!(tab = (char *)malloc(sizeof(char) * (ft_val(n) + 1))))
+	val = ft_val(n);
+	if (n < 0)
+		val++;
+	if (!(tab = (char *)malloc(sizeof(char) * (val + 1))))
 		return (NULL);
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
@@ -45,12 +65,8 @@ char			*ft_itoa(int n)
 		tab[0] = '-';
 		i++;
 		n = -n;
+		val--;
 	}
-	while (j < ft_val(n))
-	{
-		power = ft_val(n) - (j++) - 1;
-		tab[i++] = 48 + (n / (ft_power(10, power)) % 10);
-	}
-	tab[i] = '\0';
+	tab = ft_nbr(n, val, i, tab);
 	return (tab);
 }
